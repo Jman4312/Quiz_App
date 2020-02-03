f41249b3bf90ad6645c2a083c119927e48660570
jman4312 <jimmylowe@verizon.net>
2020-02-03 09:29:39 -0500

    Initial Commit- just added in the log filter, still need to declare a global variable for EditText and TextView

--- app/src/main/java/com/lowejimmy/quizapp/MainActivity.java
+++ app/src/main/java/com/lowejimmy/quizapp/MainActivity.java
@@ -3,12 +3,24 @@ package com.lowejimmy.quizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button submitButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        submitButton = findViewById(R.id.clickButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Hello mom");
+                Log.i("testButton","Hi dad!");
+            }
+        });
     }
 }
